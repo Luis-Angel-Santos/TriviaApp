78ced84d4207732d93cb48312c9e298412dc02d4
Luis Angel Santos <santosangel748@gmail.com>
2022-12-26 13:10:51 -0600

    Creando la estructura del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material/material.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { TriviaComponent } from './trivia/trivia/trivia.component';
+import { HomeComponent } from './trivia/home/home.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    TriviaComponent,
+    HomeComponent,
+    AppComponent,
   ],
   imports: [
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
